dc80d41bed253e54fce5387f7cbbaff171a56dcf
Timo Railo <timo@east.fi>
2025-05-05 14:43:32 +0300

    added project description for publishing

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+
+# Read the contents of README.md
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="llm_tester",
     version="0.1.0",
     description="A framework for testing LLM performance using pydanticAI",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # Specify content type as Markdown
     author="",
     author_email="",
     packages=find_packages(),
